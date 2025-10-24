c6788b62b1f41263172f480c6c89b72cc517161a
Arjun Arunprakash <t1q6d@ugrad.cs.ubc.ca>
2025-10-24 16:29:56 -0700

    implemented JsonReader Class

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -16,33 +16,61 @@ public class JsonReader {
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
-
+        this.source = source;
     }
 
     // EFFECTS: reads folder from file and returns it;
     // throws IOException if an error occurs reading data from file
     public Folder read() throws IOException {
-        return null;
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseFolder(jsonObject);
     }
 
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        return "";
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
     // EFFECTS: parses folder from JSON object and returns it
     private Folder parseFolder(JSONObject jsonObject) {
-        return null;
+        Folder f = new Folder();
+        addPlants(f, jsonObject);
+        return f;
     }
 
     // MODIFIES: folder
     // EFFECTS: parses plant from JSON object and adds them to folder
-    private void addPlant(Folder f, JSONObject jsonObject) {
+    private void addPlants(Folder f, JSONObject jsonObject) {
+        JSONArray jsonArray = jsonObject.getJSONArray("folder");
+        for (Object json : jsonArray) {
+            JSONObject nextPlant = (JSONObject) json;
+            addPlant(f, nextPlant);
+        }
     }
 
     // MODIFIES: folder
-    // EFFECTS: parses plants from JSON object and adds it to folder
-    private void addPlants(Folder f, JSONObject jsonObject) {
+    // EFFECTS: parses plant from JSON object and adds it to folder
+    private void addPlant(Folder f, JSONObject jsonObject) {
+        String commonName = jsonObject.getString("commonName");
+        int dateAdded = jsonObject.getInt("dateAdded");
+        String ubcLocation = jsonObject.getString("ubcLocation");
+        String speciesName = jsonObject.getString("speciesName");
+        String observations = jsonObject.getString("observations");
+        int plantId = jsonObject.getInt("plantId");
+
+        Plant plant = new Plant(commonName, dateAdded);
+        plant.setObservations(observations);
+        plant.setSpeciesName(speciesName);
+        plant.setLocationString(ubcLocation);
+        plant.setPlantId(plantId);
+        f.addPlant(plant);
     }
 
 }

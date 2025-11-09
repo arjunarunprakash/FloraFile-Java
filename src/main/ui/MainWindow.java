4fa1daa6f12ed655718b7ffae9a4a5ba2dd4305c
Arjun Arunprakash <t1q6d@ugrad.cs.ubc.ca>
2025-11-08 16:45:36 -0800

    Included class and constructor specification

--- src/main/ui/MainWindow.java
+++ src/main/ui/MainWindow.java
@@ -1,22 +1,29 @@
 package ui;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
+// Main JFrame Window for GUI
 public class MainWindow {
 
-    private JFrame window;
+    private JFrame window;   
 
+    // EFFECTS: creates new JFrame, sets title, sets exit behavior, 
+    // sets window dimensions, centers window, adds custom logo to window
     public  MainWindow() {
         window = new JFrame();
         window.setTitle("UBC Flora File");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(800,500);
         window.setLocationRelativeTo(null);
+        ImageIcon logo = new ImageIcon("logo.png");
 
     }
 
+    // MODIFIES: this
+    // EFFECT: makes window visible
     public void show(){
-        window.setVisible(true);    
+        window.setVisible(true);
     }
 
     

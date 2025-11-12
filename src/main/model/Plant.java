53d79e8accd67447b3522f9527764d1d599ceaff
Arjun Arunprakash <t1q6d@ugrad.cs.ubc.ca>
2025-11-11 22:14:03 -0800

    further refactored and better specified the plant class to include to include the updated LocalDateTime type

--- src/main/model/Plant.java
+++ src/main/model/Plant.java
@@ -12,6 +12,7 @@ public class Plant implements Writable {
     private String speciesName; // species name of plant
     private String ubcLocation; // Location of plant on UBC
     private LocalDateTime dateAdded; // Listed date found dd-MM-yyyy HH:mm:ss format
+    private String dateFormatted;
     private String observation; // Observations
     private static int entryCounter = 0; //Counts total number of entries in application
     private int plantId;
@@ -19,14 +20,14 @@ public class Plant implements Writable {
 
     /*
      * REQUIRES: name that has a non-zero length
-     * and date entered in correct YYYYMMDD format
      * EFFECTS: commonName is set to name; dateFound is set to date; plant id is
      * unique positive integer.
      * speciesName, ubcLocation, and observations are set to default values
      */
-    public Plant(String name, int date) {
+    public Plant(String name) {
         this.commonName = name;
-        this.dateAdded = date;
+        this.dateAdded = LocalDateTime.now();
+        this.dateFormatted = getDateTimeAdded(dateAdded);
         this.speciesName = noDetail;
         this.ubcLocation = noDetail;
         this.observation = noDetail;
@@ -37,15 +38,15 @@ public class Plant implements Writable {
 
     /*
      * REQUIRES: name that has a non-zero length,
-     * and date entered in correct YYYYMMDD format,
      * plantid is unique and valid parameter.
      * EFFECTS: constructs plant where all fields are manually set based on
      * parameters
      */
-    public Plant(String name, int date, String ubcL, String specName, String obs,
+    public Plant(String name, String ubcL, String specName, String obs,
             int plantId) {
         this.commonName = name;
-        this.dateAdded = date;
+        this.dateAdded = LocalDateTime.now();
+        this.dateFormatted = getDateTimeAdded(dateAdded);
         this.speciesName = specName;
         this.ubcLocation = ubcL;
         this.observation = obs;
@@ -166,8 +167,8 @@ public class Plant implements Writable {
         // stub
     }
 
-    public LocalDateTime getDateTimeAdded() {
-        // stub
+    public String getDateTimeAdded(LocalDateTime date) {
+        return "";
     }
 
 }

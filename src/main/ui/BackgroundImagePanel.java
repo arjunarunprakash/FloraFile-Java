fca9680c8347b6c13a8593de2021e37245086965
Arjun Arunprakash <t1q6d@ugrad.cs.ubc.ca>
2025-11-13 21:18:52 -0800

    specified new BackgroundImagePanel Class and associated methods

---
+++ src/main/ui/BackgroundImagePanel.java
@@ -0,0 +1,26 @@
+package ui;
+
+import java.awt.Graphics;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+// a constantly run background panel containing a background image 
+public class BackgroundImagePanel extends JPanel {
+
+    private ImageIcon backgroundImage;
+
+    //EFFECTS: sets backgroundImage to specified image location
+    public BackgroundImagePanel(String bg) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: draws image onto panel
+    @Override
+    public void paintComponent(Graphics g) {
+
+    }
+    
+
+}

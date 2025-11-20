c69c44451b59f807f66d4603dd42d9334fb5087f
Arjun Arunprakash <t1q6d@ugrad.cs.ubc.ca>
2025-11-20 00:34:37 -0800

    Implemented helper methods: saveFolder(), loadFolder(), and exitApp().

--- src/main/ui/MainFrame.java
+++ src/main/ui/MainFrame.java
@@ -1,43 +1,47 @@
 package ui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Font;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+
+import model.Folder;
+import persistence.JsonReader;
+import persistence.JsonWriter;
 
 // MainFrame is a child of JFrame, represents opening window made for GUI components
 public class MainFrame extends JFrame implements PersistenceInterface {
 
-    private Color offWhiteColour = new Color(245,242,208);
     private JPanel upperTile;
     private JLabel label;
     private JMenuBar menuBar;
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
+    private Folder plantsFolder = new Folder();
 
-    // EFFECTS: creates new JFrame this, sets title, sets exit behavior, 
-    // sets this dimensions, centers this, adds custom logo to this, 
+    // EFFECTS: creates new JFrame this, sets title, sets exit behavior,
+    // sets this dimensions, centers this, adds custom logo to this,
     // sets background colour to custom white colour
-    public  MainFrame() {
+    public MainFrame() {
         try {
-        this.setTitle("UBC Flora File");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(800,500);
-        this.setLocationRelativeTo(null);
-        this.setLayout(new BorderLayout());
-        ImageIcon logo = new ImageIcon("logo.png");
-        this.setIconImage(logo.getImage());
-        BackgroundImagePanel bg = new BackgroundImagePanel("data/Images/backgroundImage2.png");
-        this.setContentPane(bg);
-        //this.getContentPane().setBackground(offWhiteColour);
-        //upperPanel();
-        //this.add(upperTile);
-        this.setVisible(true);
+            
+            jsonWriter = new JsonWriter(PersistenceInterface.JSON_STORE);
+            jsonReader = new JsonReader(PersistenceInterface.JSON_STORE);
+            this.setTitle("UBC Flora File");
+            this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+            this.setSize(800, 500);
+            this.setLocationRelativeTo(null);
+            this.setLayout(new BorderLayout());
+            ImageIcon logo = new ImageIcon("logo.png");
+            this.setIconImage(logo.getImage());
+            BackgroundImagePanel bg = new BackgroundImagePanel("data/Images/backgroundImage2.png");
+            this.setContentPane(bg);
+            menuBar = new JMenuBar();
+            // this.getContentPane().setBackground(offWhiteColour);
+            // upperPanel();
+            // this.add(upperTile);
+            this.setVisible(true);
         } catch (Exception e) {
             System.out.println("MainFrame error");
         }
@@ -50,7 +54,7 @@ public class MainFrame extends JFrame implements PersistenceInterface {
         upperPanelLabel();
         upperTile = new JPanel();
         upperTile.setBackground(new Color(0xC3FDB8));
-        upperTile.setBounds(0,0,800,62);
+        upperTile.setBounds(0, 0, 800, 62);
         upperTile.add(label);
 
     }
@@ -59,54 +63,106 @@ public class MainFrame extends JFrame implements PersistenceInterface {
     // EFFECTS: added pictures and text to the upperTile
     public void upperPanelLabel() {
         label = new JLabel("What will you file today?");
-        label.setForeground(new Color (30,42,86));
+        label.setForeground(new Color(30, 42, 86));
         label.setFont(new Font("Brush Script MT", Font.ITALIC, 25));
 
     }
 
     // MODIFIES: this
     // EFFECTS: creates menubar on top of frame to allow user to save and load files
-    public void initializeMenuBar(){
-        menuBar = new JMenuBar();
+    public void initializeMenuBar() {
+
         JMenu file = new JMenu("File");
         JMenuItem saveFile = new JMenuItem("Save Progress");
         JMenuItem loadFile = new JMenuItem("Load Progress");
-        JMenuItem saveAndExit = new JMenuItem("Save and Exit");
+        JMenuItem Exit = new JMenuItem("Save and Exit");
+
+        menuBar.add(file);
 
         file.add(saveFile);
         file.add(loadFile);
-        file.add(saveAndExit);
+        file.add(Exit);
 
-        saveFile.addActionListener (e -> saveFolder());
-        loadFile.addActionListener (e -> loadFolder());
-        saveAndExit.addActionListener(e -> exitApp());
+        saveFile.addActionListener(e -> saveFolder());
+        loadFile.addActionListener(e -> loadFolder());
+        Exit.addActionListener(e -> exitApp());
 
     }
-    
+
     // Referenced from the JsonSerialization Demo
     // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
     // MODIFIES: this
     // EFFECTS: loads Folder from file
-    void loadFolder(){
+    void loadFolder() {
+        try {
+            plantsFolder = jsonReader.read();
 
+            JOptionPane.showMessageDialog(this, "Successfully loaded your catalog to:\n" + JSON_STORE,
+            "Load Successful",
+            JOptionPane.INFORMATION_MESSAGE);
+            System.out.println("\n-----Sucessfully Loaded your saved catalog from " + JSON_STORE + " -----");
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(
+                this,
+                "Unable to load from file:\n" + JSON_STORE,
+                "Load Failed",
+                JOptionPane.ERROR_MESSAGE
+        );
+        }
     }
 
     // Referenced from the JsonSerialization Demo
     // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
     // MODIFIES: ./data/folder.json
     // EFFECTS: saves the Folder to file
-    void saveFolder(){
+   void saveFolder() {
+    try {
+        jsonWriter.open();
+        jsonWriter.write(plantsFolder);
+        jsonWriter.close();
 
+        JOptionPane.showMessageDialog(
+                this,   
+                "Successfully saved your catalog to:\n" + JSON_STORE,
+                "Save Successful",
+                JOptionPane.INFORMATION_MESSAGE
+        );
+
+    } catch (FileNotFoundException e) {
+
+        JOptionPane.showMessageDialog(
+                this,
+                "Unable to write to file:\n" + JSON_STORE,
+                "Save Failed",
+                JOptionPane.ERROR_MESSAGE
+        );
     }
+}
 
     // Referenced from the JsonSerialization Demo
     // https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
     // MODIFIES: this
-    // EFFECTS: gives user to save application, then exits
-    void exitApp(){
+    // EFFECTS: gives user option to save application, then exits
+    void exitApp() {
+       int chosenOption = JOptionPane.showConfirmDialog(
+                this,
+                "Would you like to save before exit?",
+                "Save?",
+                JOptionPane.YES_NO_CANCEL_OPTION
+        );
+
+        switch (chosenOption) {
+            case JOptionPane.YES_OPTION:
+                saveFolder();
+                break;
+            case JOptionPane.NO_OPTION:
+                System.exit(0);
+                break;
+            case JOptionPane.CANCEL_OPTION:
+            // do nothing, just close dialog box
+                break;
+        }
 
     }
-
-
 
 }

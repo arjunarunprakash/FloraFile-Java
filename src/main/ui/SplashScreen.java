4e985311848335e7b7243f92fb88d873f514f04f
Arjun Arunprakash <t1q6d@ugrad.cs.ubc.ca>
2025-11-21 15:42:36 -0800

    Created and specified splash screen class for the visual component of GUI.
    Added specifications for class methods

---
+++ src/main/ui/SplashScreen.java
@@ -0,0 +1,19 @@
+package ui;
+
+import javax.swing.*;
+
+// A simple splash screen showing the user the app logo before entering app
+public class SplashScreen extends JWindow {
+
+    private int duration;
+
+    // EFFECTS: Constructs splashscreen instance that lasts for specified duration
+    public SplashScreen(int duration) {
+        this.duration = duration;
+    }
+    
+    // EFFECTS: sets up JPanel for window then displays Image for specified duration
+    public void displaySplash() {
+
+    }
+}

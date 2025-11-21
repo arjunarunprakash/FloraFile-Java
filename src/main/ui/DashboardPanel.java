ab2c387a8df04bb25e6ec0a823c9b536725eb0ce
Arjun Arunprakash <t1q6d@ugrad.cs.ubc.ca>
2025-11-21 12:14:44 -0800

    Created DashboardPanel class to contain a rightPanel and leftPanel. The right panel represents a add Entry feature that allows user to add entries and the left panel represent a updating list allowing user to view all plants in plantsFolder.
    Added Specification for all relevant right panel methods.

---
+++ src/main/ui/DashboardPanel.java
@@ -0,0 +1,92 @@
+package ui;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+import model.Folder;
+
+// Represents the 2 main panels making up the home screen of the app
+public class DashboardPanel extends JPanel {
+    private Folder plantsFolder;
+    private CardLayout cardLayout;
+    private JPanel leftCardPanel;
+    private JList<String> plantList;
+    
+    // Form Fields
+    private JTextField nameField;
+    private JTextField speciesField;
+    private JComboBox<String> locationBox;
+    private JTextArea obsArea;
+
+    // Colours and font
+    private final Color woodColor = new Color(233, 175, 108);
+    private final Color darkWood = new Color(91, 50, 16);
+    private final Font pixelFont = new Font("Dialog", Font.BOLD, 14);
+    private ImageIcon iconAdd; 
+
+    // EFFECTS: constructor to initialize panels and fields
+    public DashboardPanel(Folder folder){
+        this.plantsFolder = folder;
+        this.iconAdd = new ImageIcon(new ImageIcon("data/Images/NewEntryLogo.png")
+                .getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH));
+
+        initializeSettings();
+        initializeLayout();
+        refreshList();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes settings of panel to ensure the background image can be seen
+    public void initializeSettings() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add a left and right panel to current panel
+    public void initializeLayout() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: specifies and creates the left panel, passed in initialize layout
+    public void createLeftPanel(){
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: creates the Form to be displayed when left panel is clicked, to submit new entries
+    public JPanel createFormPanel(){
+        return null; //stub
+    }
+
+    // MODIFES: this
+    // EFFECTS: adds labels for each off the fields, then adds the relevant text fields to each label
+    public void addFieldsToForm (JPanel p) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a button that will allow the user to save or cancel their entry
+    public void addButtonsToForm(){
+
+    }
+
+    // MODIFIES: this, plantsFolder
+    // EFFECTS: gathers data from text field and combobox and passes it to plant model to create a new entry
+    //          also will have to clear all the text fields
+    public void submitForm(){
+    
+    }
+
+    // MODIFES: this
+    // EFFECTS: refreshs the left panel to include a list of all entries in the plantFolder;
+    //          can be used after new entry is added or after file has been loaded
+    public void refreshList(){
+
+    }
+
+
+
+
+}
